76e45f88cce65cca0070e16674e31a1b9097fb64
iwannatto <iwannatto@gmail.com>
2021-04-02 16:52:27 +0900

    Use .clang-format

--- src/common.h
+++ src/common.h
@@ -1,12 +1,12 @@
 #define PFATAL(x...)                                                           \
-	do {                                                                   \
-		fprintf(stderr, "[-] SYSTEM ERROR : " x);                      \
-		fprintf(stderr, "\n\tLocation : %s(), %s:%u\n", __FUNCTION__,  \
-			__FILE__, __LINE__);                                   \
-		perror("      OS message ");                                   \
-		fprintf(stderr, "\n");                                         \
-		exit(EXIT_FAILURE);                                            \
-	} while (0)
+    do {                                                                       \
+        fprintf(stderr, "[-] SYSTEM ERROR : " x);                              \
+        fprintf(stderr, "\n\tLocation : %s(), %s:%u\n", __FUNCTION__,          \
+                __FILE__, __LINE__);                                           \
+        perror("      OS message ");                                           \
+        fprintf(stderr, "\n");                                                 \
+        exit(EXIT_FAILURE);                                                    \
+    } while (0)
 
 /* siphash.c */
 uint32_t hsiphash_static(const void *src, unsigned long src_sz);
--- src/kcov.c
+++ src/kcov.c
@@ -17,73 +17,69 @@
 #define KCOV_TRACE_CMP 1
 
 struct kcov {
-	int fd;
-	uint64_t *cover;
+    int fd;
+    uint64_t *cover;
 };
 
-struct kcov *kcov_new(void)
-{
-	int fd = open("/sys/kernel/debug/kcov", O_RDWR);
-	if (fd == -1) {
-		PFATAL("open(/sys/kernel/debug/kcov)");
-	}
+struct kcov *kcov_new(void) {
+    int fd = open("/sys/kernel/debug/kcov", O_RDWR);
+    if (fd == -1) {
+        PFATAL("open(/sys/kernel/debug/kcov)");
+    }
 
-	/* Setup trace mode and trace size. */
-	int r = ioctl(fd, KCOV_INIT_TRACE, COVER_SIZE);
-	if (r != 0) {
-		PFATAL("ioctl(KCOV_INIT_TRACE)");
-	}
+    /* Setup trace mode and trace size. */
+    int r = ioctl(fd, KCOV_INIT_TRACE, COVER_SIZE);
+    if (r != 0) {
+        PFATAL("ioctl(KCOV_INIT_TRACE)");
+    }
 
-	/* Mmap buffer shared between kernel- and user-space. */
-	unsigned long *cover = (unsigned long *)mmap(
-		NULL, COVER_SIZE * sizeof(unsigned long),
-		PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    /* Mmap buffer shared between kernel- and user-space. */
+    unsigned long *cover =
+        (unsigned long *)mmap(NULL, COVER_SIZE * sizeof(unsigned long),
+                              PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
-	if ((void *)cover == MAP_FAILED) {
-		PFATAL("mmap(/sys/kernel/debug/kcov)");
-	}
-	struct kcov *kcov = calloc(1, sizeof(struct kcov));
-	kcov->fd = fd;
-	kcov->cover = cover;
-	return kcov;
+    if ((void *)cover == MAP_FAILED) {
+        PFATAL("mmap(/sys/kernel/debug/kcov)");
+    }
+    struct kcov *kcov = calloc(1, sizeof(struct kcov));
+    kcov->fd = fd;
+    kcov->cover = cover;
+    return kcov;
 }
 
-void kcov_enable(struct kcov *kcov)
-{
-	/* reset counter */
-	__atomic_store_n(&kcov->cover[0], 0, __ATOMIC_RELAXED);
+void kcov_enable(struct kcov *kcov) {
+    /* reset counter */
+    __atomic_store_n(&kcov->cover[0], 0, __ATOMIC_RELAXED);
 
-	int r = ioctl(kcov->fd, KCOV_ENABLE, KCOV_TRACE_PC);
-	if (r != 0) {
-		PFATAL("ioctl(KCOV_ENABLE)");
-	}
+    int r = ioctl(kcov->fd, KCOV_ENABLE, KCOV_TRACE_PC);
+    if (r != 0) {
+        PFATAL("ioctl(KCOV_ENABLE)");
+    }
 
-	/* Reset coverage. */
-	__atomic_store_n(&kcov->cover[0], 0, __ATOMIC_RELAXED);
-	__sync_synchronize();
+    /* Reset coverage. */
+    __atomic_store_n(&kcov->cover[0], 0, __ATOMIC_RELAXED);
+    __sync_synchronize();
 }
 
-int kcov_disable(struct kcov *kcov)
-{
-	/* Stop counter */
-	__sync_synchronize();
+int kcov_disable(struct kcov *kcov) {
+    /* Stop counter */
+    __sync_synchronize();
 
-	int kcov_len = __atomic_load_n(&kcov->cover[0], __ATOMIC_RELAXED);
+    int kcov_len = __atomic_load_n(&kcov->cover[0], __ATOMIC_RELAXED);
 
-	/* Stop actual couting. */
-	int r = ioctl(kcov->fd, KCOV_DISABLE, 0);
-	if (r != 0) {
-		PFATAL("ioctl(KCOV_DISABLE)");
-	}
-	return kcov_len;
+    /* Stop actual couting. */
+    int r = ioctl(kcov->fd, KCOV_DISABLE, 0);
+    if (r != 0) {
+        PFATAL("ioctl(KCOV_DISABLE)");
+    }
+    return kcov_len;
 }
 
-void kcov_free(struct kcov *kcov)
-{
-	close(kcov->fd);
-	kcov->fd = -1;
-	munmap(kcov->cover, COVER_SIZE * sizeof(unsigned long));
-	kcov->cover = MAP_FAILED;
+void kcov_free(struct kcov *kcov) {
+    close(kcov->fd);
+    kcov->fd = -1;
+    munmap(kcov->cover, COVER_SIZE * sizeof(unsigned long));
+    kcov->cover = MAP_FAILED;
 }
 
 uint64_t *kcov_cover(struct kcov *kcov) { return kcov->cover; }
